4d0f5fce00966d5451d479376753fceffae0a6bd
Jaehyung <ajtnlaka456@naver.com>
2020-11-01 08:01:40 +0900

    modified

--- simple_myshell.c
+++ simple_myshell.c
@@ -39,20 +39,42 @@ int makelist(char *s, const char *delimiters, char** list, int MAX_LIST){
 int main(int argc, char**argv){
   int i=0;
   pid_t pid;
+  int is_background;
+  int arguments_cnt;
+  int status;
   while (1) {
   	fputs(prompt, stdout);
 	fgets(cmdline, BUFSIZ, stdin);
 	cmdline[strlen(cmdline) -1] = '\0';
-  
+
+	is_background = 0;
+	arguments_cnt = makelist(cmdline, " \t", cmdvector, MAX_CMD_ARG);
+	if (strcmp(cmdvector[arguments_cnt - 1], "&") == 0) {
+		is_background = 1;
+	}
+	if (strcmp(cmdvector[0], "cd") == 0) {
+		if (chdir(cmdvector[1]) == -1)
+			printf("cd error!\n");
+		continue;
+	}
+	else if (strcmp(cmdvector[0], "exit") == 0) {
+		break;
+	}
+
+
 	switch(pid=fork()){
 	case 0:
-		makelist(cmdline, " \t", cmdvector, MAX_CMD_ARG);
+		if (is_background == 1)
+			argv[arguments_cnt - 1] = ' ';
 		execvp(cmdvector[0], cmdvector);
 		fatal("main()");
 	case -1:
   		fatal("main()");
 	default:
-		wait(NULL);
+		if (is_background == 0)
+			waitpid(pid, &status, 0;)
+		else
+			sleep(1);
 	}
   }
   return 0;
